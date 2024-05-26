0c45e56b9a08f515151ea24c8f6e112fbe73c4fa
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-05-26 17:53:48 +0300

    add

---
+++ create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS minions (
+    id serial PRIMARY KEY,
+    name VARCHAR(30),
+    age INTEGER
+);
+
+CREATE TABLE minions (
+	id serial PRIMARY KEY,
+	name varchar(30),
+	age int
+);
